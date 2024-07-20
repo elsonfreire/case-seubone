4fdae8e8192480fd7235063b4c080405c2a01993
elsonfreire <elsonfreirefilho@gmail.com>
2024-07-20 17:23:12 -0300

    Register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 
-export default function Login() {
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("salesperson");
+
   return (
     <Layout>
       <h1>Página de Cadastro de Usuário</h1>
+      <form>
+        <input type="text" placeholder="Usuário" value={username}></input>
+        <br />
+        <input type="text" placeholder="Senha" value={password}></input>
+        <br />
+        <select value={role}>
+          <option value="salesperson">Vendedor</option>
+          <option value="manager">Gerente</option>
+        </select>
+        <br />
+        <button type="submit">Entrar</button>
+      </form>
     </Layout>
   );
 }
